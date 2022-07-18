377b4437f0e553ec20cd21075146c49507cdd6c2
Grzegorz Skorupa <GSkorupa@nmg.pl>
2022-07-18 11:29:03 +0200

    wip

--- build.sh
+++ build.sh
@@ -4,6 +4,60 @@
 export SIGNOMIX_GA_TRACKING_ID=NONE
 export SIGNOMIX_STATUSPAGE_URL=/
 
+# the above variables can be overridden by local configuration
+cfg_location="$1"
+echo "$1"
+if [ -z "$cfg_location" ]
+then
+    # default configuration
+    cfg_location=local/build-images.cfg
+fi
+if [ -f "$cfg_location" ]
+then
+    echo "Building Signomix using configuration from "$cfg_location":"
+    . "$cfg_location"
+else
+    echo "Building Signomix using default config:"
+fi
+
+# printing config
+echo
+echo "versionApp=$versionApp"
+echo "versionAccount=$versionAccount"
+echo "versionDb=$versionDb"
+echo "versionMain=$versionMain"
+echo "versionMs=$versionMs"
+echo "versionProxy=$versionProxy"
+echo "versionPs=$versionPs"
+echo "versionReceiver=$versionReceiver"
+echo
+echo "imageNameApp=$imageNameApp"
+echo "imageNameAccount=$imageNameAccount"
+echo "imageNameDb=$imageNameDb"
+echo "imageNameMain=$imageNameMain"
+echo "imageNameMs=$imageNameMs"
+echo "imageNameProxy=$imageNameProxy"
+echo "imageNamePs=$imageNamePs"
+echo "imageNameReceiver=$imageNameReceiver"
+echo
+echo "signomixDomain=$signomixDomain"
+echo "statusPage=$statusPage"
+echo "dockerRepository=$dockerRepository"
+
+##
+## end CONFIGURATION
+##################################
+
+read -p "Do you want to proceed? (yes/no) " yn
+case $yn in 
+	yes ) echo ok, building ...;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo invalid response;
+        echo exiting...;
+		exit 1;;
+esac
+
 ### signomix
 cd ../signomix
 mvn clean package
