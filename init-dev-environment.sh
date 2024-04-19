b186480b93227cd76668d91e87a384ac6283018f
Grzegorz Skorupa <g.skorupa@gmail.com>
2024-04-19 23:08:35 +0200

    wip

--- init-dev-environment.sh
+++ init-dev-environment.sh
@@ -25,9 +25,4 @@ git clone https://github.com/signomix/signomix-webapp.git
 git clone https://github.com/signomix/signomix-view.git
 
 # Create required folders for Docker volumes
-mkdir -p ~/signomix/volumes/mosquitto/config
-mkdir -p ~/signomix/volumes/mosquitto/data
-mkdir -p ~/signomix/volumes/mosquitto/log
-mkdir -p ~/signomix/volumes/volume-postgres
-mkdir -p ~/signomix/volumes/volume-questdb
-git clone https://github.com/signomix/signomix-documentation.git ~/signomix/volumes/signomix-documentation
+./init-runtime-environment.sh
--- init-runtime-environment.sh
+++ init-runtime-environment.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-echo "Initializing runtime environment..."
-# Add your additional commands here
+# Script to create required folders for Docker volumes
 
+# Create required folders for Docker volumes
 mkdir -p ~/signomix/volumes/mosquitto/config
 mkdir -p ~/signomix/volumes/mosquitto/data
 mkdir -p ~/signomix/volumes/mosquitto/log
 mkdir -p ~/signomix/volumes/volume-postgres
 mkdir -p ~/signomix/volumes/volume-questdb
 git clone https://github.com/signomix/signomix-documentation.git ~/signomix/volumes/signomix-documentation
-
-echo "Runtime environment initialized."
