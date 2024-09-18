b67fbba8a93e3b6c77dd7b5edf4b17f694fca077
gskorupa <g.skorupa@gmail.com>
2024-09-18 18:30:13 +0200

    wip

--- init-dev-environment.sh
+++ init-dev-environment.sh
@@ -8,22 +8,33 @@
 cd ..
 
 # Clone Signomix repositories
+
+# services
 git clone https://github.com/signomix/signomix-ta-account.git
 git clone https://github.com/signomix/signomix-apigateway.git
 #git clone https://github.com/signomix/signomix-ta-app.git
 git clone https://github.com/signomix/signomix-auth.git
 git clone https://github.com/signomix/signomix-common.git
-git clone https://github.com/signomix/signomix-docs-website.git
 git clone https://github.com/signomix/signomix-ta-core.git
+git clone https://github.com/signomix/signomix-extensions.git
+
 git clone https://github.com/signomix/signomix-ta-jobs.git
 git clone https://github.com/signomix/signomix-ta-ms.git
 git clone https://github.com/signomix/signomix-ta-provider.git
 git clone https://github.com/signomix/signomix-ta-receiver.git
 git clone https://github.com/signomix/signomix-reports.git
 git clone https://github.com/signomix/signomix-sentinel.git
+
+# webapps
 git clone https://github.com/signomix/signomix-webapp.git
 git clone https://github.com/signomix/signomix-website.git
+git clone https://github.com/signomix/signomix-docs-website.git
 git clone https://github.com/signomix/signomix-view.git
+
+# content repositories
+git clone https://githb.com/signomix/signomix-documentation.git
+git clone https://githb.com/signomix/signomix-doc-templates.git
+git clone https://githb.com/signomix/signomix-app-news.git
 
 # Create required folders for Docker volumes
 ./init-runtime-environment.sh
